1c229e003e81f50646361d708f76762235748d18
JohnBen <john@johnben.fr>
2021-11-14 15:56:28 +0100

    ~

--- app/software_model/software_test.py
+++ app/software_model/software_test.py
@@ -3,7 +3,6 @@ import os
 def make_url():
     """ Class model for nmake download url """
 
-    print("enter in modules Test")
     return "https://dev.johnben.fr/software.json"
 
 if __name__ == "__main__":
