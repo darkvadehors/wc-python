56bd53600f07129fcdbf9b0d434c0cb6c3075ba3
JohnBen <john@johnben.fr>
2021-11-14 21:16:45 +0100

    cleaning

--- app/wc.py
+++ app/wc.py
@@ -12,6 +12,93 @@ import subprocess
 from urllib import request
 import app.software_ui.window as sui_windows
 
+def run_app():
+    """Same as name Run App
+
+        launch process:
+            - `check_version`
+            - `sui_windows.Windows_interface`
+            - `generate_url`
+            - `downloader`
+    """
+
+    # charge le Logger
+    logging.basicConfig(filename='myapp.log',
+                        level=logging.INFO,
+      format='%(asctime)s -- %(filename)s -- %(lineno)d -\
+      - %(name)s -- %(levelname)s -- %(message)s')
+
+    logging.info("check Version application from Github")
+    check_version()# TODO
+    logging.info("version Checked")
+
+    url_dict = {}
+
+    # launch windows_interface
+    sotfware_list = sui_windows.Windows_interface().result_list_to_return
+
+
+    if len(sotfware_list) < 1:
+        exit()
+
+    software_path:str = None
+    file_name:str = None
+
+    for software in sotfware_list:
+        try:
+            #  convert software liste to liste software/url
+            url_dict[software] = generate_url(software)
+            logging.info(f"convert in url ok {url_dict[software]}")
+        except:
+            logging.error(f"impossible de resoudre l'url de {software}")
+        try:
+            print("238 url_dict[software]",url_dict[software])
+            # récupréation du nom du fichier
+            url = url_dict[software]
+            logging.debug(url)
+            file_name = request.urlopen(
+                request.Request(url)).info().get_filename()
+
+            logging.info(f"récupération dossier \
+                distant du nom de fichier {file_name}")
+        except:
+            logging.error("impossible de récuéperer le nom du file")
+        try:
+            # Download file from URL
+            software_path = download(url_dict[software])
+            logging.info(f"Téchargement du fichier ok {software_path}")
+        except:
+            logging.error("impossible de download le file")
+
+    # lance l'installation
+    logging.debug(f"252 file_name {file_name}")
+    install_download_app( software_path)
+
+
+def check_version(): #TODO fonction futur a finir
+    """Check on distant repository if version as same
+    """
+
+    # recuperation sur github du hash du dernier commit
+    # et comparaison
+
+    # FIXME:
+    # a voir si on garde, code fonctionnel
+    # pour injecter la liste des softwares dans une
+    # variable en passant par un fichier tmp detruit juste après
+
+    # make tmpfile
+    with tempfile.TemporaryFile() as fp:
+        temp = tempfile.NamedTemporaryFile(prefix='wc_', suffix='.json')
+        url = 'http://dev.johnben.fr/software.json'
+        r = requests.get(url, allow_redirects=True)
+        open(temp.name, 'wb').write(r.content)
+        software_list = r.content
+        f = open(temp.name)
+        data = json.load(f)
+        f.close()
+        software_list = json.dumps(tuple(dict(data)))
+
 
 def generate_url(soft_name):
     """Start Url creation from list sofware
@@ -62,7 +149,6 @@ def software_name_extractor(software_url):
     Args:
         software_url (string): url of the software
     """
-
 
     print("64 software_url",software_url)
     # Split Url for extract extesion
@@ -175,95 +261,6 @@ def install_download_app( software_path):
         subprocess .call(f"open -a TextEdit {software_path}", shell =True)
     else:
         print("Extension non reconnu")
-
-
-def check_version(): #TODO fonction futur a finir
-    """Check on distant repository if version as same
-    """
-
-    # recuperation sur github du hash du dernier commit
-    # et comparaison
-
-    # FIXME:
-    # a voir si on garde, code fonctionnel
-    # pour injecter la liste des softwares dans une
-    # variable en passant par un fichier tmp detruit juste après
-
-    # make tmpfile
-    with tempfile.TemporaryFile() as fp:
-        temp = tempfile.NamedTemporaryFile(prefix='wc_', suffix='.json')
-        url = 'http://dev.johnben.fr/software.json'
-        r = requests.get(url, allow_redirects=True)
-        open(temp.name, 'wb').write(r.content)
-        software_list = r.content
-        f = open(temp.name)
-        data = json.load(f)
-        f.close()
-        software_list = json.dumps(tuple(dict(data)))
-
-
-def run_app():
-    """Same as name Run App
-
-        launch process:
-            - `check_version`
-            - `sui_windows.Windows_interface`
-            - `generate_url`
-            - `downloader`
-    """
-
-    # charge le Logger
-    logging.basicConfig(filename='myapp.log',
-                        level=logging.INFO,
-      format='%(asctime)s -- %(filename)s -- %(lineno)d -\
-      - %(name)s -- %(levelname)s -- %(message)s')
-
-    logging.info("check Version application from Github")
-    check_version()# TODO
-    logging.info("version Checked")
-
-    url_dict = {}
-
-    # launch windows_interface
-    sotfware_list = sui_windows.Windows_interface().result_list_to_return
-
-
-    if len(sotfware_list) < 1:
-        exit()
-
-    software_path:str = None
-    file_name:str = None
-
-    for software in sotfware_list:
-        try:
-            #  convert software liste to liste software/url
-            url_dict[software] = generate_url(software)
-            logging.info(f"convert in url ok {url_dict[software]}")
-        except:
-            logging.error(f"impossible de resoudre l'url de {software}")
-        try:
-            print("238 url_dict[software]",url_dict[software])
-            # récupréation du nom du fichier
-            url = url_dict[software]
-            logging.debug(url)
-            file_name = request.urlopen(
-                request.Request(url)).info().get_filename()
-
-            logging.info(f"récupération dossier \
-                distant du nom de fichier {file_name}")
-        except:
-            logging.error("impossible de récuéperer le nom du file")
-        try:
-            # Download file from URL
-            software_path = download(url_dict[software])
-            logging.info(f"Téchargement du fichier ok {software_path}")
-        except:
-            logging.error("impossible de download le file")
-
-    # lance l'installation
-    logging.debug(f"252 file_name {file_name}")
-    install_download_app( software_path)
-
 
 
 if __name__ == "__main__":
