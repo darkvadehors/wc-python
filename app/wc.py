8f89c20a05f47baa8fc07923f4039db449ca52df
JohnBen <john@johnben.fr>
2021-11-15 18:31:30 +0100

    add logging

--- app/wc.py
+++ app/wc.py
@@ -25,12 +25,12 @@ def run_app():
     # start Logger
     save_path = directory()
     logging.basicConfig( filename=save_path + 'wc.log',
-                        level=logging.INFO,
+                        level=logging.DEBUG,
       format='%(asctime)s -- %(filename)s -- %(lineno)d -\
 - %(name)s -- %(levelname)s -- %(message)s')
 
     logging.info("check Version application from Github")
-    check_version()# TODO
+    check_version()# TODO a faire dans un futur proche
     logging.info("version Checked")
 
     url_dict = {}
@@ -43,37 +43,55 @@ def run_app():
         exit()
 
     software_path:str = None
-    file_name:str = None
 
     for software in sotfware_list:
         try:
             #  convert software liste to liste software/url
             url_dict[software] = generate_url(software)
+            file_name = url_dict[software]
             logging.info(f"convert in url ok {url_dict[software]}")
         except:
             logging.error(f"impossible de resoudre l'url de {software}")
-        try:
-            print("57 url_dict[software]",url_dict[software])
-            # récupréation du nom du fichier
-            url = url_dict[software]
-            logging.debug(url)
-            file_name = request.urlopen(
-                request.Request(url)).info().get_filename()
+        print("55 file name", file_name)
+        print("56 url dict",url_dict[software])
+        # name = software_distant_name_resolver(url_dict[software])
+        # print("name ",name)
+#        try:
+#             # récuprération du nom du fichier
+#             url = url_dict[software]
+#             logging.debug(f"url avant recuperation du nom {url}")
+#             file_name = request.urlopen(
+#                 request.Request(url)).info().get_filename()
 
-            logging.info(f"récupération dossier \
-distant du nom de fichier {file_name}")
-        except:
-            logging.error("impossible de récuéperer le nom du file")
+#             logging.info(f"récupération du nom de fichier sur le dossier distant :\
+# { file_name }")
+#        except:
+#            logging.error("impossible de récuéperer le nom du file")
         try:
             # Download file from URL
-            software_path = download(url_dict[software])
+            # software_path = download(url_dict[software]) #FIXME: Activate downloader
             logging.info(f"Téchargement du fichier ok {software_path}")
         except:
             logging.error("impossible de download le file")
 
     # lance l'installation
-    logging.debug(f"76 file_name {file_name}")
+    logging.debug(f"file_name {file_name}")
+
     install_download_app( software_path)
+
+
+def software_distant_name_resolver(url):
+    print("83-------> ",url)
+    file_name:str = ""
+    logging.debug(f"Initialisation de File name {file_name} ")
+
+# récuprération du nom du fichier
+    logging.debug(f"url avant recuperation du nom {url}")
+    file_name = request.urlopen(request.Request(url)).info().get_filename()
+
+    logging.info(f"récupération du nom de fichier sur le dossier distant : \
+{ file_name }")
+    return file_name
 
 
 def check_version(): #TODO fonction futur a finir
@@ -115,6 +133,7 @@ def generate_url(soft_name):
 
     try:
         module = import_lib(soft_name)
+        logging.debug(f"sortie Creation import lib { module }")
         return module.make_url()
     except:
         logging.warning(f"Software Libary {soft_name} not found")
@@ -133,6 +152,7 @@ def import_lib(soft_name):
 
     # get cureent directory
     current_path = os.getcwd()
+    logging.debug(f"current_path :{current_path}")
     # make temp path for import
     spec = importlib.util.spec_from_file_location("alias" ,
             f"{current_path}/app/software_model/software_{soft_name}.py")
@@ -183,7 +203,7 @@ def download(software_url):
     """
 
     software_name = software_name_extractor(software_url)
-
+    logging.info(f"adresse retour software_name_extractor {software_name}")
     # Make object for download file
     downloaded_obj = requests.get(software_url)
 
