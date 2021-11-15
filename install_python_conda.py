8dab2072370aa243eb090bb884402553ebdf6a38
JohnBen <john@johnben.fr>
2021-11-15 15:50:51 +0100

    install and activate conda script sh

---
+++ install_python_conda.py
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Installation des modules ..."
+
+
+conda create --name nomades_env flask numpy matplotlib
+
+yes | conda install logging
+yes | conda install importlib
+yes | conda install tempfile
+yes | conda install requests
+yes | conda install importlib
+yes | conda install pathlib
+yes | conda install subprocess
+
+
+conda activate nomades_env
